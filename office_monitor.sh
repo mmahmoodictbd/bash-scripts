92cea1a17bf3b4e6a398045b61e14f1282a3da88
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2017-02-09 21:32:03 +0600

    Fix resulation for Office monitor.

---
+++ office_monitor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xrandr --newmode "1600x900_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode VGA1 1600x900_60.00
+
+xrandr --output VGA1 --mode 1600x900_60.00 --left-of LVDS1 --output LVDS1 --mode 1366x768
